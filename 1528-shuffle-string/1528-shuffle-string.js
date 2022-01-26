3327a604308e4b4f00ed31cbc5c1aefb64999588
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-01-26 18:27:39 +0530

    Time: 135 ms (18.09%), Space: 40.7 MB (41.97%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.js
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -4,8 +4,9 @@
  * @return {string}
  */
 var restoreString = function(s, indices) {
-    return indices.reduce((arr, item, i) => {
-        arr[item] = s[i];
-        return arr;
-     }, []).join('');
+    let ret = [];
+    for (let i = 0; i < indices.length; ++i) {
+        ret[indices[i]] = s.charAt(i);
+    }
+    return ret.join('');
 };

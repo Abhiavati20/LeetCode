ee278f25d962f66a78e4b8622a9018f3f51558c8
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-01 22:54:46 +0530

    Time: 109 ms (50.56%), Space: 47.5 MB (5.19%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.js
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.js
@@ -14,8 +14,7 @@ var removeElements = function(head, val) {
     if(head===null)
         return null;
     head.next=removeElements(head.next,val);
-    if(head.val === val)
-        return head.next;
     
-    return head;
+    
+    return head.val === val ? head.next : head;
 };

2ae0a8f7c14a891467b44451e725f5bd737a9138
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-07 12:22:41 +0530

    Time: 79 ms (82.44%), Space: 42 MB (27.14%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.js
+++ 389-find-the-difference/389-find-the-difference.js
@@ -1,30 +1,16 @@
-class Solution {
-public:
-    char findTheDifference(string s, string t) {
-        int n = s.length(); 
-        vector<int> freq(26, 0); 
-        
-        for(int i = 0; i < n; i++)
-        {
-            freq[s[i] - 'a']--;
-        }
-        
-        for(int i = 0; i < n + 1; i++)
-        {
-            freq[t[i] - 'a']++;
-        }
-        
-        char ans = 'a'; 
-        
-        for(int i = 0; i < 26; i++)
-        {
-            if(freq[i] == 1)
-            {
-                ans = (char) i + 'a'; 
-                break;
-            }
-        }
-        
-        return ans;  
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let char = 0;
+    let i = 0;
+    for(i = 0; i < s.length; i++ )
+    {
+        char += t.charCodeAt(i);
+        char -= s.charCodeAt(i);
     }
+    char += t.charCodeAt(i);
+    return String.fromCharCode(char);
 };

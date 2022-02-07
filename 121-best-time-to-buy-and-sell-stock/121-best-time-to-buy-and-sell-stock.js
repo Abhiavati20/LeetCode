6d10ae75bae73a32526060d4b091755a47b57af5
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-07 12:41:56 +0530

    Time: 145 ms (34.84%), Space: 51.3 MB (31.80%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-//     int daym=prices[0], result=INT_MIN, n=prices.size();
-        
-//         for(int i=1;i<n;i++) {
-            
-//             result=max(result, prices[i]-daym);
-//             daym=min(daym, prices[i]);
-//         }
-//         return result>=0 ? result : 0;
     let daym = prices[0],result = Number.MIN_VALUE, n = prices.length;
     for(let i = 1; i < n; i++)
     {

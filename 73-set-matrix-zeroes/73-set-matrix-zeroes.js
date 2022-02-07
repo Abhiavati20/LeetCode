a8c20882e79e8dba26be10ea67f3d39745606446
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-07 12:54:18 +0530

    Time: 134 ms (41.24%), Space: 44.5 MB (19.40%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -3,30 +3,6 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    // int m = matrix.size();
-    //     int n = matrix[0].size();
-    //     vector<int> row,col;
-    //     for(int i = 0; i < matrix.size(); i++)
-    //     {
-    //         for(int j = 0; j < matrix[i].size(); j++)
-    //         {    
-    //             if(matrix[i][j] == 0)
-    //             {
-    //                 row.push_back(i);
-    //                 col.push_back(j);
-    //             }
-    //         }
-    //     }
-    //     for(int i = 0 ; i < row.size() ; i++){
-    //         for(int j = 0 ; j < n ; j++){
-    //             matrix[row[i]][j] = 0;
-    //         }
-    //     }
-    //     for(int i = 0 ; i < col.size() ; i++){
-    //         for(int j = 0 ; j < m ; j++){
-    //             matrix[j][col[i]] = 0;
-    //         }
-    //     }
     let m = matrix.length;
     let n = matrix[0].length;
     let row = [],col= [];

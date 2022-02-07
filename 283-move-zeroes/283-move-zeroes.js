e7f34d877fffac8fa40e86b840f893a9094777b1
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-07 19:31:07 +0530

    Time: 88 ms (98.34%), Space: 46.3 MB (16.70%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,15 +3,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let count = 0;
-    for(let i = 0; i < nums.length; i++)
-        if(nums[i] === 0)
-            count++;
-    let ans = [];
-    for(let i = 0; i < nums.length; i++)
-        if(nums[i]) ans.push(nums[i]);
-    while(count--)
-        ans.push(0);
-    for(let i = 0; i < nums.length; i++)
-        nums[i] = ans[i]; 
+    // let count = 0;
+    // for(let i = 0; i < nums.length; i++)
+    //     if(nums[i] === 0)
+    //         count++;
+    // let ans = [];
+    // for(let i = 0; i < nums.length; i++)
+    //     if(nums[i]) ans.push(nums[i]);
+    // while(count--)
+    //     ans.push(0);
+    // for(let i = 0; i < nums.length; i++)
+    //     nums[i] = ans[i];
+    let countZero = 0;
+    for(let  i = 0; i < nums.length; i++)
+    {
+        if(nums[i] != 0)
+            nums[countZero++] = nums[i];
+    }
+    for(let i = countZero; i < nums.length; i++)
+        nums[i] = 0;
 };

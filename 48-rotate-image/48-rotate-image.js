246ae80ec0240d627dcf7d743985c229764c241b
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-09 15:28:44 +0530

    Time: 122 ms (16.47%), Space: 42.1 MB (19.67%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -4,18 +4,33 @@
  */
 var rotate = function(matrix) {
     let n = matrix.length;
-    let arr = [];
+//     let arr = [];
+//     for(let i = 0; i < n; i++)
+//     {
+//         let temp = []
+//         for(let j = i+1; j < n; j++){
+//             temp.push(matrix[j][i]);
+//         }
+//         arr.push(temp);
+//     }
+//     for(let i = 0; i < n; i++){
+//         for(let j = 0; j < n/2; j++)
+//             matrix[i][j] = arr[i][n-j-1];
+        
+//     }
     for(let i = 0; i < n; i++)
     {
-        let temp = []
-        for(let j = 0; j < n; j++){
-            temp.push(matrix[j][i]);
+        for(let j = i+1; j < n; j++){
+                let tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;    
         }
-        arr.push(temp);
     }
     for(let i = 0; i < n; i++){
-        for(let j = 0; j < n; j++)
-            matrix[i][j] = arr[i][n-j-1];
-        
+        for(let j = 0; j < n/2; j++){
+                let tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+        }
     }
 };

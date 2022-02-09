73a253fc1d7ba4cec4b6809b6613e7a263a191f1
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-09 10:57:41 +0530

    Time: 115 ms (37.13%), Space: 42.5 MB (17.38%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countKDifference = function(nums, k) {
+    let count = 0;
+    for(let i = 0; i < nums.length; i++)
+    {
+        for(let j = 0; j < nums.length; j++){
+            if(nums[j]-nums[i] == k)
+                count++;
+        }
+    
+    }
+    return count;
+};

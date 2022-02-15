a8539b59c388e1815760bb95a81d83331d678ebb
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-15 10:01:35 +0530

    Time: 20 ms (76.93%), Space: 17 MB (50.83%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> mp;
-        for(auto i : nums)
-            mp[i]++;
-        for(auto i : nums)
+        
+        /* BRUTE FORCE */         
+        
+        // map<int,int> mp;
+        // for(auto i : nums)
+        //     mp[i]++;
+        // for(auto i : nums)
+        // {
+        //     if(mp[i] == 1)
+        //         return i;
+        // }
+        
+        /* BIT MANIPULATION */
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(mp[i] == 1)
-                return i;
+            ans = ans ^ nums[i];
         }
-        return 0;
+        
+        return ans;
     }
 };

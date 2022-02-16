a1d2843d5a072045e0fa435f491f41405c5f7c5f
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-16 10:15:11 +0530

    Time: 3 ms (70.43%), Space: 7.6 MB (55.41%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,23 +11,35 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode * p = head,*q;
-        ListNode *dummy = new ListNode(0);
+        /* NAIVE APPROACH */
         
-        while(p!=NULL && p->next!=NULL)
-        {
+//         ListNode * p = head,*q; // pointers for iteration
+//         ListNode *dummy = new ListNode(0);  // dummy node which help to swap nodes and change the value of head
+// //         looping till we get null || till we get last odd node
+//         while(p!=NULL && p->next!=NULL)
+//         {
             
-            q = p->next;
-            if(dummy->next == NULL) head = p->next;
-            dummy -> next = p -> next;
+//             q = p->next;    
+//             if(dummy->next == NULL) head = p->next;
+//             dummy -> next = p -> next;
             
-            p->next=q->next;
-            q->next=p;
-            dummy = p;
-            p=p->next;
+//             p->next=q->next;
+//             q->next=p;
+//             dummy = p;
+//             p=p->next;
             
-        }
-        return head;
+//         }
+//         return head;
         
+        /* RECURSIVE */
+        if(head==NULL || head->next ==NULL)
+            return head;
+    
+            ListNode *curr = head, *nextNode=head->next;
+            ListNode *nextele = swapPairs(curr->next->next);
+            nextNode->next=curr;
+            curr->next=nextele;
+    
+            return nextNode;
     }
 };

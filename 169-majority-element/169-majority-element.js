4f094f8ecaac6e26c5e48f4427cb44fd31085966
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-02-21 09:53:16 +0530

    Time: 113 ms (38.69%), Space: 45 MB (17.06%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,13 +3,19 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let mp = new Map();
-    for(let i = 0; i < nums.length; i++){
-        let count = mp.get(nums[i]) || 0;
-        mp.set(nums[i],++count);
-    }
-    for(let i = 0; i < nums.length; i++){
-        if(mp.get(nums[i]) > (nums.length/2))return nums[i];
-    }
-    return 0;
+    // let mp = new Map();
+    // for(let i = 0; i < nums.length; i++){
+    //     let count = mp.get(nums[i]) || 0;
+    //     mp.set(nums[i],++count);
+    // }
+    // for(let i = 0; i < nums.length; i++){
+    //     if(mp.get(nums[i]) > (nums.length/2))return nums[i];
+    // }
+    // return 0;
+    
+    // space complexity O(1)
+    
+    nums.sort((a,b)=>a-b);
+    let mid = Math.trunc(nums.length / 2);
+    return nums[mid];
 };

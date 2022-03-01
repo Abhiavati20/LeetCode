556b2a591fd564b759c565ad9cff6dc23a71cd4d
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-03-01 09:35:50 +0530

    Time: 3 ms (95.58%), Space: 7.8 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -1,18 +1,25 @@
-/**
- * @param {number} n
- * @return {number[]}
- */
-var countBits = function(n) {
-    let count = 0;
-    let arr = [];
-    for(let i = 0; i <= n; i++){
-        let j = i;
-        count = 0;
-        while(j){
-            if(j & 1) count++;
-            j >>= 1;
+class Solution {
+public:
+    vector<int> countBits(int n) {
+//         O(n^2)
+        // int count = 0;
+        // vector<int> res;
+        // for(int i = 0; i <= n; i++){
+        //     int j = i;
+        //     count = 0;
+        //     while(j){
+        //         if(j & 1)
+        //             count++;
+        //         j>>=1;
+        //     }
+        //     res.push_back(count);
+        // }
+//         O(n)
+        vector<int> res(n+1);
+        res[0] = 0;
+        for(int i = 0; i <= n; i++){
+            res[i] = res[i / 2] + i % 2;
         }
-        arr.push(count);
+        return res;
     }
-    return arr;
 };

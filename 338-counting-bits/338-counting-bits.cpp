bca7ac69f5bf68e7e004eb7d68bac56846c14a54
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-03-01 09:40:45 +0530

    Time: 3 ms (95.58%), Space: 7.8 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -18,8 +18,8 @@ public:
         vector<int> res(n+1);
         res[0] = 0;
         for(int i = 0; i <= n; i++){
-            // res[i] = res[i / 2] + i % 2;
-            res[i] = res[i>>1] + (i & 1);
+            res[i] = res[i / 2] + i % 2;
+            // res[i] = res[i>>1] + (i & 1);
         }
         return res;
     }

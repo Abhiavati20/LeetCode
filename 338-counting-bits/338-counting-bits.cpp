d2b2d48670b39d98be93795f287520db55a269b6
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-03-01 09:39:52 +0530

    Time: 7 ms (62.32%), Space: 7.8 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        int count = 0;
-        vector<int> res;
+//         O(n^2)
+        // int count = 0;
+        // vector<int> res;
+        // for(int i = 0; i <= n; i++){
+        //     int j = i;
+        //     count = 0;
+        //     while(j){
+        //         if(j & 1)
+        //             count++;
+        //         j>>=1;
+        //     }
+        //     res.push_back(count);
+        // }
+//         O(n)
+        vector<int> res(n+1);
+        res[0] = 0;
         for(int i = 0; i <= n; i++){
-            int j = i;
-            count = 0;
-            while(j){
-                if(j & 1)
-                    count++;
-                j>>=1;
-            }
-            res.push_back(count);
+            // res[i] = res[i / 2] + i % 2;
+            res[i] = res[i>>1] + (i & 1);
         }
         return res;
     }

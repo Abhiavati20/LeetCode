ab57e7016e111c9f97144e39e277d6bd47b3b03c
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-03-20 16:11:28 +0530

    Time: 177 ms (58.00%), Space: 111.5 MB (72.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
+    int dominoRotations(vector<int>& tops, vector<int>& bottoms)
+    {
+        int minRotation = INT_MAX;
+        for(int i = 1; i <= 6; ++i)
+        {
+            int swap = 0 , j;
+            for(j = 0; j<tops.size(); ++j)
+                {
+                    if(tops[j] == i)
+                        continue;
+                    else if(bottoms[j] == i)
+                        ++swap;
+                    else
+                        break;
+                }
+                if(j == tops.size() && minRotation > swap)
+                    minRotation = swap;
+        }
+        return minRotation == INT_MAX ? -1 : minRotation;
+    }
+    
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        vector<int> cnt(7, 0);
-        int mn = 1e9, n = tops.size();
-        
-        for(int i = 0; i < n; i++) {
-            cnt[tops[i]] += 1;
-            if(tops[i] != bottoms[i])
-                cnt[bottoms[i]] += 1;
-        }
-        
-        for(int i = 1; i <= 6; i++) {
-            if(cnt[i] < n) continue;
-            int cnt1 = 0, cnt2 = 0;
-            for(int j = 0; j < n; j++) {
-                if(tops[j] != i) cnt1 += 1;
-                if(bottoms[j] != i) cnt2 += 1;
-            }
-            mn = min({mn, cnt1, cnt2});
-        }
-        
-        return mn == 1e9 ? -1 : mn;
+        int ab = dominoRotations(tops,bottoms);
+        int ba = dominoRotations(bottoms,tops);
+        return ab == -1 ? ba : ba == -1 ? ab : min(ab,ba);
     }
 };

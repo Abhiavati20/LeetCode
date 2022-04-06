e74e06058164a4acf86772d5abc0b08337b4f7f6
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-04-06 17:56:58 +0530

    Time: 1866 ms (5.11%), Space: 10.8 MB (20.41%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,71 +1,29 @@
 class Solution {
 public:
-    int threeSumMulti(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
+    long long mod = 1e9 + 7;
+    
+    int threeSumMulti(vector<int>& arr, int target) {
         
-        // two pointer approach 
-		
-        long long mod=1e9+7;
-		
-        int n=nums.size();
-		
-        int ans=0;
-		
-        long long sum;
-		
-        for(int i=0;i<n-2;i++)
+        int n = arr.size();
+        
+        unordered_map<int, int> mp;
+        
+        int count = 0;
+        
+        for(int i = 0; i < n; i++)
         {
-            int l=i+1;
-			
-            int r=n-1;
-            
-            while(l<r)
+            for(int j = i + 1; j < n; j++)
             {
-                sum=nums[l]+nums[i]+nums[r];
-             
-                if(sum==target)
-                {
-                      int count1=1;
-                      int count2=1;
-                    while(l<r && nums[l]==nums[l+1])
-                    {
-                        l++;
-                        count1++;
-                    }
-                    
-                    while(l<r && nums[r]==nums[r-1])
-                    {
-                        r--;
-                        count2++;
-                    }
-                                           
-                    if(l==r)         
-                    {                                  
-                        ans = (ans%mod + (count1%mod *(count1-1)%mod)/2)%mod;
-                    }
-                    
-                    if(l!=r)
-                    {
-                        ans = (ans%mod + (count1%mod* count2%mod)%mod)%mod;	 
-                    }
-                    
-                    l++;
-                    r--;
-                }
-                else
-                {
-                    if(sum>target)
-                    {
-                        r--;
-                    }
-                    else
-                    {
-                        l++;
-                    }
-                }
+                int need = target - arr[i] - arr[j];
+                
+                count += mp[need];
             }
+            
+            count %= mod;
+            
+            mp[arr[i]]++;
         }
         
-        return ans;
+        return count % mod;
     }
 };

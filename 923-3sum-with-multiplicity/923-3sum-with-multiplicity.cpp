745dc84ff88b83927e6be0b2ad41f2b36e135f1b
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-04-06 17:55:29 +0530

    Time: 434 ms (45.58%), Space: 10.5 MB (74.83%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,15 +1,71 @@
 class Solution {
 public:
-    int threeSumMulti(vector<int>& arr, int target) {
+    int threeSumMulti(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        
+        // two pointer approach 
+		
+        long long mod=1e9+7;
+		
+        int n=nums.size();
+		
         int ans=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                ans += mp[target-arr[i]-arr[j]];
+		
+        long long sum;
+		
+        for(int i=0;i<n-2;i++)
+        {
+            int l=i+1;
+			
+            int r=n-1;
+            
+            while(l<r)
+            {
+                sum=nums[l]+nums[i]+nums[r];
+             
+                if(sum==target)
+                {
+                      int count1=1;
+                      int count2=1;
+                    while(l<r && nums[l]==nums[l+1])
+                    {
+                        l++;
+                        count1++;
+                    }
+                    
+                    while(l<r && nums[r]==nums[r-1])
+                    {
+                        r--;
+                        count2++;
+                    }
+                                           
+                    if(l==r)         
+                    {                                  
+                        ans = (ans%mod + (count1%mod *(count1-1)%mod)/2)%mod;
+                    }
+                    
+                    if(l!=r)
+                    {
+                        ans = (ans%mod + (count1%mod* count2%mod)%mod)%mod;	 
+                    }
+                    
+                    l++;
+                    r--;
+                }
+                else
+                {
+                    if(sum>target)
+                    {
+                        r--;
+                    }
+                    else
+                    {
+                        l++;
+                    }
+                }
             }
-            ans =ans%1000000007;
-            mp[arr[i]]++;
         }
-        return ans%1000000007;
+        
+        return ans;
     }
 };

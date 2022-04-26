47a8c8f767a7e223bdcd6d323c709cfadab6b5f7
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-04-26 18:36:20 +0530

    Time: 437 ms (62.86%), Space: 58.3 MB (57.91%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,51 +1,80 @@
+class UnionFind {
+public:
+    vector<int> group;
+    vector<int> rank;
+
+    UnionFind(int size) {
+        group = vector<int>(size);
+        rank = vector<int>(size);
+        for (int i = 0; i < size; ++i) {
+            group[i] = i;
+        }
+    }
+
+    int find(int node) {
+        if (group[node] != node) {
+            group[node] = find(group[node]);
+        }
+        return group[node];
+    }
+
+    bool join(int node1, int node2) {
+        int group1 = find(node1);
+        int group2 = find(node2);
+        
+        // node1 and node2 already belong to same group.
+        if (group1 == group2) {
+            return false;
+        }
+
+        if (rank[group1] > rank[group2]) {
+            group[group2] = group1;
+        } else if (rank[group1] < rank[group2]) {
+            group[group1] = group2;
+        } else {
+            group[group1] = group2;
+            rank[group2] += 1;
+        }
+
+        return true;
+    }
+};
+
+
 class Solution {
 public:
-    int parents[1001];
-     
-   int findParents( int a){
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<pair<int, pair<int, int>>> allEdges;
         
-       if(parents[a]<0)
-           return a;
-       
-           return parents[a] = findParents(parents[a]);
-   }
-   
-   void makeParents(int a, int b){
-       parents[a] =b;
-   }
-         
-   int minCostConnectPoints(vector<vector<int>>& points) {
-             int n = points.size();
-          for(int i=0; i<n; i++) parents[i]= -1;
-         
-       vector<pair<int , pair<int , int>>>adj;
-        
-       for(int i=0; i<n; i++){
-           for(int j=i+1; j<n; j++){
-               
-               int weight = abs(points[i][0] - points[j][0]) +
-                             abs(points[i][1] - points[j][1]);
-               
-                adj.push_back({weight, {i,j}});
-           }
-       }
-       
-       sort(adj.begin(), adj.end());
-        
-       int sum =0;
-       
-       for(int i=0; i<adj.size(); i++){
-           
-            int a = findParents(adj[i].second.first);
-            int b = findParents(adj[i].second.second);
-            
-           if(a!=b){
-               sum += adj[i].first;
+        // Storing all edges of our complete graph.
+        for (int currNode = 0; currNode < n; ++currNode) {
+            for (int nextNode = currNode + 1; nextNode < n; ++nextNode) {
+                int weight = abs(points[currNode][0] - points[nextNode][0]) + 
+                             abs(points[currNode][1] - points[nextNode][1]);
                 
-               makeParents(a,b);
-           }
-       }
-       
-       return sum;
-   }
+                allEdges.push_back({ weight, { currNode, nextNode }});
+            }
+        }
+        
+        // Sort all edges in increasing order.
+        sort(allEdges.begin(), allEdges.end());
+        
+        UnionFind uf(n);
+        int mstCost = 0;
+        int edgesUsed = 0;
+        
+        for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; ++i) {
+            int node1 = allEdges[i].second.first;
+            int node2 = allEdges[i].second.second;
+            int weight = allEdges[i].first;
+            
+            if (uf.join(node1, node2)) {
+                mstCost += weight;
+                edgesUsed++;
+            }
+        }
+        
+        return mstCost;
+    }
 };

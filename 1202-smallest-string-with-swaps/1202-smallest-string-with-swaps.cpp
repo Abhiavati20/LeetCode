89d473c8be11093906f76c169fd19f5201f51976
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-04-27 18:26:30 +0530

    Time: 329 ms (27.98%), Space: 57.1 MB (38.25%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,48 +1,131 @@
-class Solution {
-public:
-    bool flag[100011];
-    vector<int> vec[100001], pos;
-    vector<char> allChars;
-    string str;
-    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        int n = pairs.size();
-        for(int i=0;i<n;i++)flag[i] = false;
-        str = s;
-        for(int i=0;i<n;i++){
-            int u = pairs[i][0];
-            int v = pairs[i][1];
+// class Solution {
+// public:
+//     bool flag[100011];
+//     vector<int> vec[100001], pos;
+//     vector<char> allChars;
+//     string str;
+//     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+//         int n = pairs.size();
+//         for(int i=0;i<n;i++)flag[i] = false;
+//         str = s;
+//         for(int i=0;i<n;i++){
+//             int u = pairs[i][0];
+//             int v = pairs[i][1];
             
-            vec[u].push_back(v);
-            vec[v].push_back(u);
-        }
+//             vec[u].push_back(v);
+//             vec[v].push_back(u);
+//         }
         
-        for(int i=0;i<n;i++){
-            if(!flag[i]){
-                pos.clear();
-                allChars.clear();
-                dfs(i);
+//         for(int i=0;i<n;i++){
+//             if(!flag[i]){
+//                 pos.clear();
+//                 allChars.clear();
+//                 dfs(i);
                 
-                sort(allChars.begin(), allChars.end());
-                sort(pos.begin(), pos.end());
+//                 sort(allChars.begin(), allChars.end());
+//                 sort(pos.begin(), pos.end());
                 
-                for(int j=0;j<pos.size();j++){
-                    str[ pos[j] ] = allChars[j];
-                }
-            }
-        }
+//                 for(int j=0;j<pos.size();j++){
+//                     str[ pos[j] ] = allChars[j];
+//                 }
+//             }
+//         }
         
-        return str;
+//         return str;
+//     }
+    
+//     void dfs(int nd){
+//         flag[nd] = true;
+//         allChars.push_back(str[nd]);
+//         pos.push_back(nd);
+        
+//         for(auto v: vec[nd]){
+//             if(!flag[v]){
+//                 dfs(v);
+//             }
+//         }
+//     }
+// };
+class UnionFind {
+private:
+    vector<int> root;
+    vector<int> rank;
+public:
+    // Initialize the array root and rank
+    // Each vertex is representative of itself with rank 1
+    UnionFind(int sz) : root(sz), rank(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
     }
     
-    void dfs(int nd){
-        flag[nd] = true;
-        allChars.push_back(str[nd]);
-        pos.push_back(nd);
-        
-        for(auto v: vec[nd]){
-            if(!flag[v]){
-                dfs(v);
+    // Get the root of a vertex
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    // Perform the union of two components
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] >= rank[rootY]) {
+                root[rootY] = rootX;
+                rank[rootX] += rank[rootY];
+            } else {
+                root[rootX] = rootY;
+                rank[rootY] += rank[rootX];
             }
         }
+    }
+};
+
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        UnionFind uf(s.size());
+        
+        // Iterate over the edges
+        for (vector<int> edge : pairs) {
+            int source = edge[0];
+            int destination = edge[1];
+            
+            // Perform the union of end points
+            uf.unionSet(source, destination);
+        }
+        
+        
+        unordered_map<int, vector<int>> rootToComponent;
+        // Group the vertices that are in the same component
+        for (int vertex = 0; vertex < s.size(); vertex++) {
+            int root = uf.find(vertex);
+            // Add the vertices corresponding to the component root
+            rootToComponent[root].push_back(vertex);
+        }
+        
+        // String to store the answer
+        string smallestString(s.length(), ' ');
+        // Iterate over each component
+        for (auto component : rootToComponent) {
+            vector<int> indices = component.second;
+            
+            // Sort the characters in the group
+            vector<char> characters;
+            for (int index : indices) {
+                characters.push_back(s[index]);
+            }
+            sort(characters.begin(), characters.end());
+            
+            // Store the sorted characters
+            for (int index = 0; index < indices.size(); index++) {
+                smallestString[indices[index]] = characters[index];
+            }
+        }
+        
+        return smallestString;
     }
 };

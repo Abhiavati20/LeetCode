3fc59db0673356ebd200e2c771aabcac50a0544a
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-05-10 14:12:09 +0530

    Time: 3 ms (96.74%), Space: 8.8 MB (54.12%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -6,21 +6,36 @@ public:
         return -1;
     }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        for(int i = 0; i < nums1.size(); i++)
+        // vector<int> res;
+        // for(int i = 0; i < nums1.size(); i++)
+        // {
+        //     int j = index(nums2,nums1[i]),k;
+        //     int greater = -1;
+        //     for(k = j + 1; k < nums2.size(); k++)
+        //     {
+        //         if(nums1[i] < nums2[k])
+        //         {
+        //             greater = nums2[k];
+        //             break;
+        //         }
+        //     }    
+        //     res.push_back(greater);
+        // }
+        // return res;
+        unordered_map<int,int> mp;
+        stack<int> st;
+        vector<int> ans;
+        for(int i = nums2.size() - 1; i >= 0; i--)
         {
-            int j = index(nums2,nums1[i]),k;
-            int greater = -1;
-            for(k = j + 1; k < nums2.size(); k++)
-            {
-                if(nums1[i] < nums2[k])
-                {
-                    greater = nums2[k];
-                    break;
-                }
-            }    
-            res.push_back(greater);
+            while(!st.empty() && st.top() < nums2[i]) st.pop();
+            if(st.empty()) mp[nums2[i]] = -1;
+            else mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
         }
-        return res;
+        for(auto x : nums1)
+        {
+            ans.push_back(mp[x]);    
+        }
+        return ans;
     }
 };

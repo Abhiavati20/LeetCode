ec553767ba988593cf8379d1f918c5e12e0c2ba8
Cyrus <67231117+Abhiavati20@users.noreply.github.com>
2022-08-02 00:07:46 +0530

    Time: 144 ms (81.10%), Space: 61.3 MB (27.84%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -9,11 +9,26 @@ public:
         // return -1;
         
 //         brute force 2
-        vector<int> vec(nums.size(), 0);
-        for(int i = 0; i < nums.size(); ++i) {
-            vec[nums[i]]++;
+        // vector<int> vec(nums.size(), 0);
+        // for(int i = 0; i < nums.size(); ++i) {
+        //     vec[nums[i]]++;
+        // }
+        // for(int i = 0; i < vec.size(); ++i) if(vec[i] > 1) return i;
+        // return -1;
+        
+//         optimal solution 
+        int fast = nums[0];
+        int slow = nums[slow];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow!=fast);
+        
+        fast = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        for(int i = 0; i < vec.size(); ++i) if(vec[i] > 1) return i;
-        return -1;
+        return slow;
     }
 };
